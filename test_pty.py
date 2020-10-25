c34b6e2cb936de377fd5d950d561409b1386ff90
Soumendra Ganguly <soumendraganguly@gmail.com>
2020-10-24 22:14:30 -0500

    Minor update to test_pty.py
    Signed-off-by: Soumendra Ganguly <soumendraganguly@gmail.com>

--- test_pty.py
+++ test_pty.py
@@ -103,6 +103,7 @@ class PtyTest(unittest.TestCase):
         # signal: just ignore the signal.
         pass
 
+    #@expectedFailure
     def test_openpty(self):
         try:
             mode = tty.tcgetattr(pty.STDIN_FILENO)
