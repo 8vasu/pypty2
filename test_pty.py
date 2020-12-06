d2da61c2c485f4967d0e46fb8519efbeb1a811d2
Soumendra Ganguly <soumendraganguly@gmail.com>
2020-12-06 04:17:21 -0600

    Add test_winch().
    Signed-off-by: Soumendra Ganguly <soumendraganguly@gmail.com>

--- test_pty.py
+++ test_pty.py
@@ -77,6 +77,20 @@ def expectedFailureIfStdinIsTTY(fun):
         pass
     return fun
 
+if tty.HAVE_WINSZ and hasattr(signal, "SIGWINCH"):
+    HAVE_WINCH = True
+else:
+    HAVE_WINCH = False
+
+def expectedFailureIfStdinIsTTYAndHAVEWINCH(fun):
+    # avoid isatty()
+    if HAVE_WINCH:
+        try:
+            tty.tcgetattr(pty.STDIN_FILENO)
+            return unittest.expectedFailure(fun)
+        except tty.error:
+            pass
+    return fun
 
 # XXX(nnorwitz):  these tests leak fds when there is an error.
 class PtyTest(unittest.TestCase):
@@ -104,8 +118,12 @@ class PtyTest(unittest.TestCase):
             except OSError:
                 pass
 
+        if HAVE_WINCH:
+            old_winch = signal.getsignal(signal.SIGWINCH)
+            self.addCleanup(signal.signal, signal.SIGWINCH, old_winch)
+
     def handle_sig(self, sig, frame):
-        self.fail("isatty hung")
+        self.fail("isatty() hung")
 
     @staticmethod
     def handle_sighup(signum, frame):
@@ -132,7 +150,7 @@ class PtyTest(unittest.TestCase):
                 # Modify pty.STDIN_FILENO window size; we need to
                 # check if pty.openpty() is able to set pty slave
                 # window size accordingly.
-                debug("Setting pty.STDIN_FILENO window size")
+                debug("setting pty.STDIN_FILENO window size")
                 debug(f"original winsize: {self.old_stdin_winsz}")
                 target_stdin_winsz = tty.winsize()
                 target_stdin_winsz.ws_row = self.old_stdin_winsz.ws_row + 1
@@ -148,18 +166,18 @@ class PtyTest(unittest.TestCase):
                 self.assertEqual(new_stdin_winsz, target_stdin_winsz,
                                  "pty.STDIN_FILENO window size unchanged")
             except OSError:
-                warnings.warn("Failed to set pty.STDIN_FILENO window size")
+                warnings.warn("failed to set pty.STDIN_FILENO window size")
 
         try:
-            debug("Calling pty.openpty()")
+            debug("calling pty.openpty()")
             try:
                 master_fd, slave_fd = pty.openpty(mode, new_stdin_winsz)
             except TypeError:
                 master_fd, slave_fd = pty.openpty()
-            debug(f"Got master_fd '{master_fd}', slave_fd '{slave_fd}'")
+            debug(f"got master_fd '{master_fd}', slave_fd '{slave_fd}'")
         except OSError:
             # " An optional feature could not be imported " ... ?
-            raise unittest.SkipTest("Pseudo-terminals (seemingly) not functional.")
+            raise unittest.SkipTest("pseudo-terminals (seemingly) not functional")
 
         self.assertTrue(os.isatty(slave_fd), "slave_fd is not a tty")
 
@@ -191,13 +209,13 @@ class PtyTest(unittest.TestCase):
             # Restore the original flags.
             os.set_blocking(master_fd, blocking)
 
-        debug("Writing to slave_fd")
+        debug("writing to slave_fd")
         os.write(slave_fd, TEST_STRING_1)
         s1 = _readline(master_fd)
         self.assertEqual(b'I wish to buy a fish license.\n',
                          normalize_output(s1))
 
-        debug("Writing chunked output")
+        debug("writing chunked output")
         os.write(slave_fd, TEST_STRING_2[:5])
         os.write(slave_fd, TEST_STRING_2[5:])
         s2 = _readline(master_fd)
@@ -215,35 +233,35 @@ class PtyTest(unittest.TestCase):
         if pid == pty.CHILD:
             # stdout should be connected to a tty.
             if not os.isatty(1):
-                debug("Child's fd 1 is not a tty?!")
+                debug("child's fd 1 is not a tty?!")
                 os._exit(3)
 
             # After pty.fork(), the child should already be a session leader.
             # (on those systems that have that concept.)
-            debug("In child, calling os.setsid()")
+            debug("in child, calling os.setsid()")
             try:
                 os.setsid()
             except OSError:
                 # Good, we already were session leader
-                debug("Good: OSError was raised.")
+                debug("OK: OSError was raised")
                 pass
             except AttributeError:
                 # Have pty, but not setsid()?
-                debug("No setsid() available?")
+                debug("no setsid() available?")
                 pass
             except:
                 # We don't want this error to propagate, escaping the call to
                 # os._exit() and causing very peculiar behavior in the calling
                 # regrtest.py !
                 # Note: could add traceback printing here.
-                debug("An unexpected error was raised.")
+                debug("an unexpected error was raised.")
                 os._exit(1)
             else:
                 debug("os.setsid() succeeded! (bad!)")
                 os._exit(2)
             os._exit(4)
         else:
-            debug("Waiting for child (%d) to finish." % pid)
+            debug("waiting for child (%d) to finish." % pid)
             # In verbose mode, we have to consume the debug output from the
             # child or the child will block, causing this test to hang in the
             # parent's waitpid() call.  The child blocks after a
@@ -272,15 +290,15 @@ class PtyTest(unittest.TestCase):
 
             (pid, status) = os.waitpid(pid, 0)
             res = os.waitstatus_to_exitcode(status)
-            debug("Child (%d) exited with code %d (status %d)." % (pid, res, status))
+            debug("child (%d) exited with code %d (status %d)" % (pid, res, status))
             if res == 1:
-                self.fail("Child raised an unexpected exception in os.setsid()")
+                self.fail("child raised an unexpected exception in os.setsid()")
             elif res == 2:
-                self.fail("pty.fork() failed to make child a session leader.")
+                self.fail("pty.fork() failed to make child a session leader")
             elif res == 3:
-                self.fail("Child spawned by pty.fork() did not have a tty as stdout")
+                self.fail("child spawned by pty.fork() did not have a tty as stdout")
             elif res != 4:
-                self.fail("pty.fork() failed for unknown reasons.")
+                self.fail("pty.fork() failed for unknown reasons")
 
             ##debug("Reading from master_fd now that the child has exited")
             ##try:
@@ -294,14 +312,14 @@ class PtyTest(unittest.TestCase):
 
     def test_master_read(self):
         """Tests pty.spawn() copy loop exit conditions."""
-        debug("Calling pty.openpty()")
+        debug("calling pty.openpty()")
         master_fd, slave_fd = pty.openpty()
-        debug(f"Got master_fd '{master_fd}', slave_fd '{slave_fd}'")
+        debug(f"got master_fd '{master_fd}', slave_fd '{slave_fd}'")
 
-        debug("Closing slave_fd")
+        debug("closing slave_fd")
         os.close(slave_fd)
 
-        debug("Reading from master_fd")
+        debug("reading from master_fd")
         try:
             data = os.read(master_fd, 1)
         except OSError: # Linux
@@ -309,6 +327,69 @@ class PtyTest(unittest.TestCase):
 
         os.close(master_fd)
         self.assertEqual(data, b"")
+
+    #@expectedFailureIfStdinIsTTYAndHAVEWINCH
+    def test_winch(self):
+        """Test pty.spawn()'s terminal window
+        resizing AFTER creation of the pty pair."""
+        if (self.old_stdin_winsz != None) and HAVE_WINCH:
+            # change this to True to make the test
+            # pass when stdin is a tty and HAVE_WINCH
+            handle_winch = False
+
+            debug("calling pty.openpty()")
+            master_fd, slave_fd = pty.openpty()
+            debug(f"got master_fd '{master_fd}', slave_fd '{slave_fd}'")
+
+            # Setup SIGWINCH handler.
+            if handle_winch:
+                wsz = tty.winsize()
+                def _hwinch(signum, frame):
+                    """SIGWINCH handler."""
+                    wsz.tcgetwinsize(pty.STDIN_FILENO)
+                    debug(f"SIGWINCH handler: current stdin winsize = {wsz}")
+                    debug(f"SIGWINCH handler: setting slave window size")
+                    wsz.tcsetwinsize(slave_fd)
+
+                try:
+                    # Raises ValueError if not called from main thread.
+                    signal.signal(signal.SIGWINCH, _hwinch)
+                except ValueError:
+                    self.fail("failed to install SIGWINCH handler")
+
+            # Set pty slave window size to be the same as
+            # the original window size of pty.STDIN_FILENO.
+            self.old_stdin_winsz.tcsetwinsize(slave_fd)
+
+            # Modify pty.STDIN_FILENO window size; we need to check
+            # if the pty slave window size gets adjusted accordingly;
+            # this is the responsibility of the SIGWINCH handler.
+            new_stdin_winsz = None
+            try:
+                debug("setting pty.STDIN_FILENO window size")
+                debug(f"original winsize: {self.old_stdin_winsz}")
+                target_stdin_winsz = tty.winsize()
+                target_stdin_winsz.ws_row = self.old_stdin_winsz.ws_row + 1
+                target_stdin_winsz.ws_col = self.old_stdin_winsz.ws_col + 1
+                target_stdin_winsz.ws_xpixel = self.old_stdin_winsz.ws_xpixel + 1
+                target_stdin_winsz.ws_ypixel = self.old_stdin_winsz.ws_ypixel + 1
+                debug(f"target winsize: {target_stdin_winsz}")
+                target_stdin_winsz.tcsetwinsize(pty.STDIN_FILENO)
+
+                # Were we able to set the window size of
+                # pty.STDIN_FILENO successfully?
+                new_stdin_winsz = tty.winsize(fd=pty.STDIN_FILENO)
+                self.assertEqual(new_stdin_winsz, target_stdin_winsz,
+                                 "pty.STDIN_FILENO window size unchanged")
+            except OSError:
+                warnings.warn("failed to set pty.STDIN_FILENO window size")
+
+            if new_stdin_winsz:
+                self.assertEqual(tty.winsize(fd=slave_fd), new_stdin_winsz,
+                                 "openpty() failed to set slave window size")
+
+            os.close(slave_fd)
+            os.close(master_fd)
 
 class SmallPtyTests(unittest.TestCase):
     """These tests don't spawn children or hang."""
